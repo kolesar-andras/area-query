9fafb9fe05c4263df1515439f1560c79ee64fbe1
Kolesár András <kolesar@kolesar.hu>
2016-04-27 14:01:46 +0200

    added polygon labels

--- map.js
+++ map.js
@@ -4,7 +4,31 @@ function initMap () {
 	console.log(ol);
 
 	selected = new ol.layer.Vector({
-		source: getSource()
+		source: getSource(),
+		style: (function() {
+			var stroke = new ol.style.Stroke({
+				color: 'black'
+			});
+			var textStroke = new ol.style.Stroke({
+				color: '#fff',
+				width: 3
+			});
+			var textFill = new ol.style.Fill({
+				color: '#000'
+			});
+			return function(feature, resolution) {
+				return [new ol.style.Style({
+					stroke: stroke,
+					text: new ol.style.Text({
+						font: '12px Calibri,sans-serif',
+						text: feature.get('name'),
+						fill: textFill,
+						stroke: textStroke
+					})
+				})];
+			};
+		 })()
+
 	});
 
 	var map = new ol.Map({
